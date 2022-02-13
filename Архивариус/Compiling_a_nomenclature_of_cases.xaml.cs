d89b286a5f4c2f9fbeb0fb1aee823f8ebdf51964
gr682_gnm <gr682_gnm@STUDVM-5>
2022-02-13 12:31:02 +0700

    Добавьте файлы проекта.

---
+++ Архивариус/Compiling_a_nomenclature_of_cases.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Архивариус
+{
+    /// <summary>
+    /// Логика взаимодействия для Compiling_a_nomenclature_of_cases.xaml
+    /// </summary>
+    public partial class Compiling_a_nomenclature_of_cases : Window
+    {
+        public Compiling_a_nomenclature_of_cases()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Архивариус/Сompilation_of_nomenclature_cases.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Архивариус
+{
+    /// <summary>
+    /// Логика взаимодействия для Сompilation_of_nomenclature_cases.xaml
+    /// </summary>
+    public partial class Сompilation_of_nomenclature_cases : Window
+    {
+        public Сompilation_of_nomenclature_cases()
+        {
+            InitializeComponent();
+        }
+    }
+}
